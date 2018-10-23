fe08cfdbd4ef27c6b7411310fac3aeaa1fd3a5c4
신재순 <sjslsm@gmail.com>
2018-10-23 14:17:35 +0900

    Initial commit

---
+++ app/src/main/java/com/sjs/edu/mylinearlayout/MainActivity.java
@@ -0,0 +1,31 @@
+package com.sjs.edu.mylinearlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.button1:
+                Toast.makeText(getApplicationContext(),"버튼1이 눌렸습니다. ",Toast.LENGTH_SHORT).show();
+
+                break;
+            case R.id.button2:
+                Toast.makeText(getApplicationContext(),"버튼2이 눌렸습니다. ",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button3:
+                Toast.makeText(getApplicationContext(),"버튼3이 눌렸습니다. ",Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+    }
+
+}
